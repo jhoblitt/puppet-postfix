505c95a12c9623808d8a552d967cef51af6b1010
Joshua Hoblitt <jhoblitt@cpan.org>
2014-05-21 17:19:37 -0700

    fix string quoting issue + linter warnings

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,24 +1,24 @@
 class postfix {
   include ::postfix::params
 
-  File {
-    owner => $postfix::params::owner,
-    group => $postfix::params::group,
-    mode  => '0644',
-  }
-
-  package { [ "postfix", "mailx" ]:
+  package { [ 'postfix', 'mailx' ]:
     ensure => present,
   } ->
   # this master file breaks cyrus/etc. on rhel4
-  file { "/etc/postfix/master.cf":
-    ensure  => present,
-    source  => "puppet:///modules/postfix/${postfix::params::postfix_master_src",
-    notify  => Service['postfix'],
+  file { '/etc/postfix/master.cf':
+    ensure => present,
+    owner  => $::postfix::params::owner,
+    group  => $::postfix::params::group,
+    mode   => '0644',
+    source => "puppet:///modules/postfix/${::postfix::params::postfix_master_src}",
+    notify => Service['postfix'],
   } ->
   file { '/etc/postfix/main.cf':
     ensure  => present,
-    content => template($postfix::params::postfix_main_erb),
+    owner   => $::postfix::params::owner,
+    group   => $::postfix::params::group,
+    mode    => '0644',
+    content => template($::postfix::params::postfix_main_erb),
     notify  => Service['postfix'],
   } ->
   service { 'postfix':
